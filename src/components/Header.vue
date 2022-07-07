4797082cd16ca539f79cf16b42c07213a327d404
Darren Yu <denyoyu6450@gmail.com>
2022-07-07 23:09:23 +0800

    bs title schedule 50%

---
+++ src/components/Header.vue
@@ -0,0 +1,71 @@
+<script>
+export default {};
+</script>
+<template>
+  <header>
+    <div class="head">
+      <div class="head_left">
+        <div class="head_tit">Backend System</div>
+        <div class="head_btns">
+          <div class="btn">
+            <router-link to="/">Permission</router-link>
+          </div>
+          <div class="btn">
+            <router-link to="/commodity">Commodity</router-link>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div class="head_right">
+        <div class="head_user"></div>
+        <div class="head_signout"></div>
+      </div>
+    </div>
+  </header>
+</template>
+<style lang="scss" scoped>
+.head {
+  background: #0275b1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .head_left {
+    display: flex;
+    .head_tit {
+      font-size: 36px;
+      color: #fff;
+      padding: 20px;
+      border-right: 1px solid #fff;
+    }
+    .head_btns {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+
+      .btn {
+        text-transform: uppercase;
+        font-size: 20px;
+
+        a {
+          padding: 30px;
+          color: #fff;
+          &.router-link-exact-active {
+            padding: 30px;
+            background: #46a3ff;
+          }
+        }
+      }
+      .btn + .btn {
+        margin-left: 20px;
+      }
+    }
+  }
+  .head_right {
+    .head_user {
+    }
+    .head_signout {
+    }
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Permission from "@/views/Permission.vue";
+import Commodity from "@/views/Commodity.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "permission",
+    component: Permission,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/commodity",
+    name: "commodity",
+    component: Commodity,
+  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
