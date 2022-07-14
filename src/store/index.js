99db1d14e7f9433defbfba19a34ffb2c5d37fe09
Darren Yu <denyoyu6450@gmail.com>
2022-07-14 23:31:04 +0800

    add vuex js

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { createStore } from "vuex";
+import state from "./state.js";
+import actions from "./actions.js";
+import mutations from "./mutations.js";
+import getters from "./getters.js";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state,
+  actions,
+  mutations,
+  getters,
 });
