6699b82e86a46759cd4154582edfafe035a9785f
Darren Yu <denyoyu6450@gmail.com>
2022-07-26 23:04:39 +0800

    add el-ui plus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import { createStore } from "vuex";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -9,8 +9,7 @@ import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import "element-plus/dist/index.css";
 
 const app = createApp(App);
-
-app.use(store).use(ElementPlus).use(router).mount("#app");
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
+app.use(store).use(ElementPlus).use(router).mount("#app");
