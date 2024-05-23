4db8a65427432a29384923be8ee35aac76ea26ca
DarrenYu <105658482+YuDarren@users.noreply.github.com>
2024-05-23 17:16:17 +0800

    api變更

--- src/api/apiTaiewan.js
+++ src/api/apiTaiewan.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const useTaiwanArea = axios.create({
-  baseURL: "https://vue-lessons-api.herokuapp.com/",
+  baseURL: "https://vue-lessons-api.vercel.app/",
 });
 //台灣縣市鄉鎮區api
-export const apiGetTaiwanArea = () => useTaiwanArea.get("/city/list");
+export const apiGetTaiwanArea = () => useTaiwanArea.get("/courses/list");
 //  baseURL: "https://demeter.5fpro.com/tw/zipcodes.json"
